ef485fa08f678a7c98dd74686089caa58e77d21d
Adeola Oladeji <deolaoladeji@gmail.com>
2022-01-18 14:07:35 +0100

    pixel quantization

--- src/africa/semicolon/chapterSeven/sieveOfEratosthenes/SieveOfEratosthenes.java
+++ src/africa/semicolon/chapterSeven/sieveOfEratosthenes/SieveOfEratosthenes.java
@@ -1,5 +1,18 @@
 package africa.semicolon.chapterSeven.sieveOfEratosthenes;
 
 public class SieveOfEratosthenes {
-    
+    private final boolean[] isPrimeNumber = new boolean[5];
+
+    public void displaySieveOfEratosthenes() {
+        for (boolean prime : isPrimeNumber) {
+//            prime = true;
+            System.out.print(prime + " ");
+        }
+    }
+
+    public void checkForPrimeIndex() {
+        for (int row = 0; row < isPrimeNumber.length; row++) {
+//            if (row)
+        }
+    }
 }
---
+++ src/africa/semicolon/chapterSeven/sieveOfEratosthenes/SieveOfEratosthenesMain.java
@@ -0,0 +1,9 @@
+package africa.semicolon.chapterSeven.sieveOfEratosthenes;
+
+public class SieveOfEratosthenesMain {
+    public static void main(String[] args) {
+        SieveOfEratosthenes sieveOfEratosthenes = new SieveOfEratosthenes();
+        sieveOfEratosthenes.checkForPrimeIndex();
+        sieveOfEratosthenes.displaySieveOfEratosthenes();
+    }
+}
