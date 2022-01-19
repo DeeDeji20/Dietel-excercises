8ede0d1fdbb78f77210ef13cbb18fc861a7ee0a5
Adeola Oladeji <deolaoladeji@gmail.com>
2022-01-19 22:35:13 +0100

    updated airline reservation

--- src/africa/semicolon/chapterSeven/airlineReservation/AirlineReservation.java
+++ src/africa/semicolon/chapterSeven/airlineReservation/AirlineReservation.java
@@ -3,6 +3,8 @@ package africa.semicolon.chapterSeven.airlineReservation;
 import java.util.Scanner;
 
 public class AirlineReservation {
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
         boolean[] seatIsEmpty = new boolean[10];
         for (int i = 0; i < 10; i++) {
@@ -29,18 +31,19 @@ public class AirlineReservation {
     private static void seatIsReserved(int seatNumber, boolean[] seatIsEmpty, int classForReservation) {
         while (seatIsEmpty[seatNumber]){
             if (seatIsEmpty[0] && seatIsEmpty[1] && seatIsEmpty[2] && seatIsEmpty[3]&& seatIsEmpty[4]){
-                System.out.println("All first class seats are taken");
+                System.out.println("All first class seats are taken\nwould you like a reservation at economy class?");
+                String response = scan.nextLine();
+                if (response.toUpperCase().charAt(0) == 'Y')
+                break;
+            }else if(seatIsEmpty[5] && seatIsEmpty[6] && seatIsEmpty[7] && seatIsEmpty[8] && seatIsEmpty[9]){
+                System.out.println("All economy class seats are taken\nwould you like a reservation at first class?");
                 break;
             }else{
                 System.out.println("Seat taken");
                 seatNumber = assignSeatNumbers(classForReservation);
             }
-
 //            break;
-
         }
-
-
         if(!seatIsEmpty[seatNumber]) seatIsEmpty[seatNumber] = true;
     }
 
@@ -59,7 +62,6 @@ public class AirlineReservation {
     }
 
     public static int collectUserInput(String message) {
-        Scanner scan = new Scanner(System.in);
         System.out.println(message);
         return scan.nextInt();
     }
