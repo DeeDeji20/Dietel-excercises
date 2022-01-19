62f5c53913b08f503682ccc00020dc4a57a3c156
Adeola Oladeji <deolaoladeji@gmail.com>
2022-01-19 20:30:31 +0100

    sieve of eratosthenes

--- src/africa/semicolon/chapterSeven/sieveOfEratosthenes/SieveOfEratosthenes.java
+++ src/africa/semicolon/chapterSeven/sieveOfEratosthenes/SieveOfEratosthenes.java
@@ -1,18 +1,24 @@
 package africa.semicolon.chapterSeven.sieveOfEratosthenes;
 
 public class SieveOfEratosthenes {
-    private final boolean[] isPrimeNumber = new boolean[5];
+    private final boolean[] isPrimeNumber = new boolean[20];
 
+    public void compute(int row){
+            isPrimeNumber[row] = true;
+    }
     public void displaySieveOfEratosthenes() {
         for (boolean prime : isPrimeNumber) {
-//            prime = true;
             System.out.print(prime + " ");
         }
     }
 
     public void checkForPrimeIndex() {
-        for (int row = 0; row < isPrimeNumber.length; row++) {
-//            if (row)
+        int squareRoot = 0;
+        int index;
+        for (index = 0; index < isPrimeNumber.length; index++) {
+            squareRoot = (int)Math.pow(index, 2);
+            boolean indexIsPrime = index >= 3 && (index % 2!=0 && index%3 != 0 && index % squareRoot!=0);
+            if(indexIsPrime) compute(index);
         }
     }
 }
