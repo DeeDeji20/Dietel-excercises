abaf79ab479287fa93468e915307206e53585f18
Adeola Oladeji <deolaoladeji@gmail.com>
2022-01-22 23:46:47 +0100

    chapter eight

---
+++ src/africa/semicolon/chapterFour/binaryToDecimal/BinaryToDecimal.java
@@ -0,0 +1,40 @@
+package africa.semicolon.chapterFour.binaryToDecimal;
+
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("Enter Binary number");
+
+        int userInput=scan.nextInt();
+        String convertUserInput= String.valueOf(userInput);
+
+        int counter=0;
+        int base=10;
+        int sum=0;
+
+        double binaryStore;
+
+        int inputLength= convertUserInput.length();
+        // System.out.println(userInput/1000);
+
+        while(counter<inputLength){
+            //check the input
+            binaryStore= userInput %(Math.pow(base, counter+1))/(Math.pow(base, counter));
+            // System.out.println(binaryStore);
+            int binaryStoreValue= (int) binaryStore;
+
+            if(binaryStoreValue ==0 || binaryStoreValue==1){
+                sum+= binaryStoreValue*(Math.pow(2, counter));
+            }else{
+                System.out.println("Not a binary digit");
+            }
+            counter++;
+        }
+
+        System.out.println(sum);
+    }
+}
