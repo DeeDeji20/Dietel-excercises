c73b35969844a589cdd4eeb881e2c67cde48c0a5
Adeola Oladeji <deolaoladeji@gmail.com>
2022-02-26 18:04:34 +0100

    credit card validation

--- src/africa/semicolon/assignments/creditCardNumberValidation/CreditCardValidation.java
+++ src/africa/semicolon/assignments/creditCardNumberValidation/CreditCardValidation.java
@@ -3,6 +3,7 @@ package africa.semicolon.assignments.creditCardNumberValidation;
 public class CreditCardValidation {
     private String number ;
 
+
     public CreditCardValidation(String number) {
         if (isValidNumberLength(number) && isValidFirstNumber(number)) this.number = number;
 
@@ -22,18 +23,71 @@ public class CreditCardValidation {
         StringBuilder stringOfDoubleEvenDigits = new StringBuilder();
         for (int i = number.length()-1; i >=0; i--){
             if (number.length() % 2 == 0 && i % 2 == 0) {
-                doubleEvenDigit = Character.getNumericValue(number.charAt(i) * 2) ;
+                doubleEvenDigit = getDoubleEvenDigit(number, stringOfDoubleEvenDigits, i);
+                stringOfDoubleEvenDigits.append(doubleEvenDigit);
             }
             if (number.length() % 2 != 0 && i % 2 != 0){
-                doubleEvenDigit = Character.getNumericValue(number.charAt(i) * 2) ;
+                doubleEvenDigit = getDoubleEvenDigit(number, stringOfDoubleEvenDigits, i);
+                stringOfDoubleEvenDigits.append(doubleEvenDigit);
             }
-            stringOfDoubleEvenDigits.append(doubleEvenDigit);
         }
         return stringOfDoubleEvenDigits.toString();
     }
 
-    private static boolean validatDoubleOfEvenDigits(int doubleEvenDigit){
+    private static int getDoubleEvenDigit(String number, StringBuilder stringOfDoubleEvenDigits, int i) {
+        int doubleEvenDigit = 0;
+        doubleEvenDigit = Character.getNumericValue(number.charAt(i)) * 2 ;
+        if (isValidDoubleOfEvenDigit(doubleEvenDigit)) return doubleEvenDigit;
+        else {
+            String str = String.valueOf(doubleEvenDigit);
+            doubleEvenDigit = Character.getNumericValue(str.charAt(0)) +
+                    Character.getNumericValue(str.charAt(1));
+                return doubleEvenDigit;
+        }
+    }
+
+    private static boolean isValidDoubleOfEvenDigit(int doubleEvenDigit){
         return doubleEvenDigit < 10;
+    }
+
+    public static int sumOfDoubleEvenPlace(String doubleEvenDigit) {
+        int sum = 0;
+        for (int i = 0; i < doubleEvenDigit.length(); i++) {
+            sum += Character.getNumericValue(doubleEvenDigit.charAt(i));
+        }
+        return sum;
+    }
+
+    public static String doubleOddPlace(String number) {
+        StringBuilder stringOfDoubleEvenDigits = new StringBuilder();
+        for (int i = number.length()-1; i >= 0; i--) {
+            //if the number is an even number
+            int doubleOddNumber;
+            if (number.length() % 2 == 0 && i % 2 != 0){
+                doubleOddNumber = getDoubleOddPlace(number, i);
+                stringOfDoubleEvenDigits.append(doubleOddNumber);
+            }
+            else if (number.length() % 2 != 0 && i % 2 != 0){
+                doubleOddNumber = getDoubleOddPlace(number, i);
+                stringOfDoubleEvenDigits.append(doubleOddNumber);
+            }
+        }
+            return stringOfDoubleEvenDigits.toString();
+    }
+
+    private static int getDoubleOddPlace(String number, int i) {
+        int doubleOddPlace = Character.getNumericValue(number.charAt(i)) * 2;
+
+        if(isValidDoubleOfOddDigit(doubleOddPlace)) return doubleOddPlace;
+        else{
+            String str = String.valueOf(doubleOddPlace);
+            doubleOddPlace = Character.getNumericValue(str.charAt(0)) + Character.getNumericValue(str.charAt(1));
+            return doubleOddPlace;
+        }
+    }
+
+    private static boolean isValidDoubleOfOddDigit(int number) {
+        return number < 10;
     }
 
     public String getNumber() {
--- test/africa/semicolon/assignments/creditCardNumberValidation/CreditCardValidationTest.java
+++ test/africa/semicolon/assignments/creditCardNumberValidation/CreditCardValidationTest.java
@@ -7,19 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CreditCardValidationTest {
     CreditCardValidation creditCard;
+    private static String doubleEvenDigit;
     @BeforeEach
     void setUp(){
         creditCard = new CreditCardValidation("4388576018402626");
     }
     @Test
     void test_that_class_is_not_null(){
-//        CreditCardValidation creditCard = new CreditCardValidation("4388576018402626");
         assertNotNull(creditCard);
     }
 
     @Test
     void test_that_card_has_digits(){
-//        CreditCardValidation creditCard = new CreditCardValidation("4388576018402626");
         String number = creditCard.getNumber();
         assertEquals("4388576018402626", number);
     }
@@ -47,20 +46,6 @@ class CreditCardValidationTest {
 //    }
 
     @Test
-    public void test_that_when_every_second_digit_from_right_can_be_added(){
-        //given that i have  a credit card number
-//        CreditCardValidation creditCard = new CreditCardValidation("3788576018402626");
-        String number = creditCard.getNumber();
-
-        //when i try to get the second number from right to left
-        for (int i = number.length()-1; i >=0; i--){
-            if (i%2 == 0) System.out.println(number.charAt(i));
-//            assertEquals(2, number.charAt(number.length()-1));
-        }
-//        assertEquals(2, creditCard.doubleSecondDigits());
-    }
-
-    @Test
     void test_that_creditcard_number_has_digits_greater_than_13_and_less_than_16(){
         CreditCardValidation.isValidNumberLength(creditCard.getNumber());
         assertTrue(creditCard.getNumber().length()>=13 && creditCard.getNumber().length()<=16);
@@ -71,14 +56,25 @@ class CreditCardValidationTest {
         assertTrue(CreditCardValidation.isValidFirstNumber(creditCard.getNumber()));
     }
 
-//    @Test
-//    void test_that_number_is_valid(){
-//
-//    }
-
 
     @Test
     void test_double_even_places(){
-        assertEquals("22823178",CreditCardValidation.doubleEvenPlace(creditCard.getNumber()));
+        assertEquals("44823178",CreditCardValidation.doubleEvenPlace(creditCard.getNumber()));
+    }
+
+    @Test
+    void test_add_all_single_digit_from_the_double_even_placed_digit(){
+        doubleEvenDigit = CreditCardValidation.doubleEvenPlace(creditCard.getNumber());
+        assertEquals(37, CreditCardValidation.sumOfDoubleEvenPlace(doubleEvenDigit));
+    }
+
+    @Test
+    void test_double_odd_places() {
+        assertEquals("33070576", CreditCardValidation.doubleOddPlace(creditCard.getNumber()));
+    }
+
+    @Test
+    void test_add_all_single_digit_from_the_double_odd_placed_digit(){
+
     }
 }
