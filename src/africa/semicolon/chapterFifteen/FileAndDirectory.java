9a6f764cd0ca2bc7a64d0a0741d5c6e50c380b35
Adeola Oladeji <deolaoladeji@gmail.com>
2022-03-27 06:39:23 +0100

    chapter fifteen

--- src/africa/semicolon/chapterFifteen/FileAndDirectory.java
+++ src/africa/semicolon/chapterFifteen/FileAndDirectory.java
@@ -1,4 +1,35 @@
 package africa.semicolon.chapterFifteen;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class FileAndDirectory {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter file or directory name");
+        Path path = Paths.get(scan.nextLine());
+
+        if (Files.exists(path)){
+            System.out.printf("%n%s exists %n", path.getFileName());
+            System.out.printf("%s a directory%n ", Files.isDirectory(path)?"Is ": "Is not");
+            System.out.printf("%s an absolute path%n", path.isAbsolute()?"Is":"Is not");
+            System.out.printf("Last modified: %s%n", Files.getLastModifiedTime(path));
+            System.out.printf("Size: %s%n", Files.size(path));
+            System.out.printf("Path:%s%n", path);
+            System.out.printf("Absolute path: %s%n", path.toAbsolutePath());
+
+            if (Files.isDirectory(path)){
+                System.out.println("%n Directory contents: %n");
+                DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path);
+                for (Path child : directoryStream){
+                    System.out.println(child);
+                }
+            }
+            else System.out.printf("%s does not exist", path);
+        }
+    }
 }
