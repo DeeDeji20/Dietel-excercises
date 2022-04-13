5254fbe2b02fed138726e4e74bdf9b99809be0f9
Adeola Oladeji <deolaoladeji@gmail.com>
2022-04-13 06:12:47 +0100

    next

--- src/africa/semicolon/algo/WordFormation.java
+++ src/africa/semicolon/algo/WordFormation.java
@@ -3,6 +3,41 @@ package africa.semicolon.algo;
 import java.util.Stack;
 
 public class WordFormation {
+    /**
+     * You are given a list of strings ops where each element is either:
+     * <p>
+     * A non-negative integer that should be pushed into a stack
+     * "POP" meaning pop the top element in the stack
+     * "DUP" meaning duplicate the top element in the stack
+     * "+" meaning pop the top two and push the sum
+     * "-" meaning pop the top two and push top - second
+     * Return the top element in the stack after applying all operations. If there are any invalid operations, return -1.
+     * <p>
+     * Constraints
+     * <p>
+     * 1 ≤ n ≤ 100,000 where n is the length of ops
+     * <p>
+     * Example 1
+     * Input
+     * ops = ["1", "5", "DUP", "3", "-"]
+     * Output: -2
+     * Explanation
+     * Following the operations:
+     * <p>
+     * We push 1 into the stack: [1]
+     * We push 5 into the stack: [1, 5]
+     * We duplicate the top element: [1, 5, 5]
+     * We push 3 into the stack: [1, 5, 5, 3]
+     * We pop 3 and 5 and push their difference 3 - 5: [1, 5, -2]
+     * We return the top element which is -2
+     * <p>
+     * Example 2
+     * Input
+     * ops = ["+"]
+     * Output: -1
+     * Explanation
+     * There's no elements in the stack so this is invalid.
+     */
     Stack<Integer> stack = new Stack<>();
     public int solution(String[] ops) {
         for (String op: ops) {
--- src/africa/semicolon/chapterSixteen/DuplicateElimination.java
+++ src/africa/semicolon/chapterSixteen/DuplicateElimination.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class DuplicateElimination {
     public static void main(String[] args) {
-        int[] arr = {9, 4, 5, 7, 8};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+//        int[] arr = {9, 4, 5, 7, 8};
+//        Arrays.sort(arr);
+//        System.out.println(Arrays.toString(arr));
     }
 }
